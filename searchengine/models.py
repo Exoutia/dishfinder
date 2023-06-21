245e08b77d6b8d76cdab9ed207c802cc1a9bc5e0
Exoutia <jhav365@gmail.com>
2023-06-21 22:20:22 +0530

    creating the basic model for the app

--- searchengine/models.py
+++ searchengine/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    restaurant_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=300)
+    locaton = models.CharField(max_length=350)
+    items = models.JSONField()
+    latitude = models.DecimalField(max_digits=15, decimal_places=10)
+    longitude = models.DecimalField(max_digits=15, decimal_places=10)
+    full_details = models.JSONField()
+
+    class meta:
+        db_table = 'restaurant'
+        
