d21db7cdcd6bbc8661a5f48f86e06d3e3a205d1c
Exoutia <jhav365@gmail.com>
2023-06-22 12:53:38 +0530

    adding searching functionalties

--- searchengine/views.py
+++ searchengine/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from .models import Restaurant
 # Create your views here.
+
+def index(request):
+    if request.method=='POST':
+        res = []
+        items_that_matched = []
+        search_term = request.POST.get('dish')
+        if search_term == '':
+            res = Restaurant.objects.all()
+            return render(request, 'index.html', {'res': res})
+
+        list_of_items = Restaurant.objects.values_list('restaurant_id', 'items')
+        for id, dic in list_of_items:
+            for key in dic.keys():
+                if search_term in dic:
+                    items_that_matched.append(key)
+                    res.append(id)
+                    break
+                search_term = search_term.lower()
+                if search_term in key.lower().split():
+                    items_that_matched.append(key)
+                    res.append(id)
+                    break
+        # print(items_that_matched)
+        result = [Restaurant.objects.get(pk=i) for i in res]
+        return render(request, 'index.html', {'res': result,'items_that_matched': items_that_matched})
+
+    res = Restaurant.objects.all()
+    return render(request, 'index.html', {'res': res})
