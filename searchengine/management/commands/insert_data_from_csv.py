b27497cbfce92c01c781a2be69b5550e174147eb
Exoutia <jhav365@gmail.com>
2023-06-22 08:24:00 +0530

    formatting the python files

--- searchengine/management/commands/insert_data_from_csv.py
+++ searchengine/management/commands/insert_data_from_csv.py
@@ -11,30 +11,35 @@ first delete the db.sqlite3 file to destroy the database.
 Then, run `python manage.py migrate` for a new empty
 database with tables"""
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     # Show this when the user types help
     help = "Loads data from children.csv"
 
     def handle(self, *args: Any, **options: Any) -> str | None:
-
-
         if Restaurant.objects.exists():
-            print('data is already  present in the database')
+            print("data is already  present in the database")
             print(ALREDY_LOADED_ERROR_MESSAGE)
             return
 
-        print("loading the data", end='')
+        print("loading the data", end="")
 
-        data_df = pd.read_csv('restaurants_small.csv')
+        data_df = pd.read_csv("restaurants_small.csv")
 
         for _, i in data_df.iterrows():
             restaurant_id, name, location, items, lat_long, full_details = i
             items = json.loads(items)
             full_details = json.loads(full_details)
-            latitude, longitude = list(map(float, lat_long.split(',')))
-            x= Restaurant(restaurant_id=restaurant_id, name=name, location=location, items=items, latitude=latitude, longitude=longitude, full_details=full_details)
+            latitude, longitude = list(map(float, lat_long.split(",")))
+            x = Restaurant(
+                restaurant_id=restaurant_id,
+                name=name,
+                location=location,
+                items=items,
+                latitude=latitude,
+                longitude=longitude,
+                full_details=full_details,
+            )
             x.save()
-            print('.', end='')
-        print('done')
-
+            print(".", end="")
+        print("done")
--- searchengine/models.py
+++ searchengine/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Restaurant(models.Model):
     restaurant_id = models.IntegerField(primary_key=True)
@@ -11,5 +12,4 @@ class Restaurant(models.Model):
     full_details = models.JSONField()
 
     class meta:
-        db_table = 'restaurant'
-
+        db_table = "restaurant"
